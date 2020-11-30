add7251c31989cb0171378cd29c730c2ec767ce0
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-11-30 21:10:47 +0100

    Send added vote message

--- src/events/message.event.js
+++ src/events/message.event.js
@@ -5,6 +5,7 @@ module.exports = {
   async execute(client, message) {
     // Prévenir des message de bots
     if (message.author.bot === true) return;
+    console.log("execute -> message.author", message.author);
 
     // Message contenant le préfix
     if (!message.content.startsWith(Config.prefix)) return;
@@ -17,9 +18,7 @@ module.exports = {
 
     // Vérifie si la commande existe
     const cmd = client.commands.get(COMMAND);
-    if (!cmd) return message.channel.send("Commande introuvable.");
-
-    // Qu'elle ne soit pas blacklist
+    if (!cmd) message.channel.send("Commande introuvable.");
 
     // Si des paramètres sont passés
     if (args.length === 0) {
@@ -29,7 +28,18 @@ module.exports = {
     // Vérifie si l'utilisateur existe
     const mentionnedMember = message.mentions.members.first();
     if (!mentionnedMember) message.channel.send("Utilisateur introuvable.");
+    console.log("execute -> mentionnedMember", mentionnedMember);
 
-    cmd.execute(message, args);
+    // Envoie le message de vote
+    sentMessage = await message.channel.send(
+      `${message.author} souhaite ${cmd.description} à ${mentionnedMember}`
+    );
+
+    // Ajoute les options de vote
+    sentMessage.react("👍");
+    sentMessage.react("👎");
+
+    // Execute la commande
+    // cmd.execute(message, args);
   },
 };
